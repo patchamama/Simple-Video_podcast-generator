25daf1eef8f826f67ad331aea28ccac4f922657c
Armando Urquiola Cabrera <patchamama.go@gmail.com>
2023-03-31 09:29:24 +0000

    Add data var examples in js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,21 @@
 console.log("alert in js...");
+
+let data = [
+    { 
+        path: "doc/example1",
+        audios: ["audio1.mp3", "audio2.mp3"],
+        images: ["image001.jpg","image002.jpg","image003.jpg","image004.jpg","image005.jpg",
+                 "image006.jpg","image007.jpg","image008.jpg","image009.jpg","image010.jpg",
+                 "image011.jpg","image012.jpg","image013.jpg"]
+    },
+    { 
+        path: "doc/example2",
+        audios: ["audio1.mp3", "audio2.mp3"],
+        images: ["image001.jpg","image002.jpg","image003.jpg","image004.jpg","image005.jpg",
+                 "image006.jpg","image007.jpg","image008.jpg","image009.jpg","image010.jpg",
+                 "image011.jpg","image012.jpg","image013.jpg"]
+    }
+];
+
+console.log(data);
+alert(data[0].path);
